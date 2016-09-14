75db7e7171b1ff5ded1e1c67f78af30b3381e271
trinityXmontoya <trinityXmontoya@gmail.com>
2016-09-14 00:06:38 -0400

    update license -> Creative Commons Attribution-NonCommercial-ShareAlike

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject tinder-finder "0.1.0-SNAPSHOT"
   :description "See README"
-  :url "http://example.com/FIXME"
-  :license {:name "MIT"}
+  :url "https://github.com/trinityXmontoya/tinder-finder"
+  :license {:name "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International"
+            :url "https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode"
+            :author "Trinity Montoya"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.1.18"]
                  [cheshire "5.6.3"]
